521234a1a2970f62696fc9be0855126ba191270a
huoxudong125 <423huo@163.com>
2018-01-13 09:23:30 +0800

    Add javaDemos Project

---
+++ collection/src/test/java/TestHashMapProblem.java
@@ -0,0 +1,12 @@
+import org.hqf.tutorials.java.collection.HashMapProblem;
+import org.junit.Test;
+
+public class TestHashMapProblem {
+
+
+    @Test
+    public void TestHashMap_HighCPU_Multithread() {
+        new HashMapProblem();
+    }
+
+}
