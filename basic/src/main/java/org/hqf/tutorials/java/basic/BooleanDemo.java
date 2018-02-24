07245d14df8c3defe2a8a9799b62cc83d2870d68
huoxudong125 <423huo@163.com>
2018-02-24 18:08:39 +0800

    add boolean Demo

---
+++ basic/src/main/java/org/hqf/tutorials/java/basic/BooleanDemo.java
@@ -0,0 +1,21 @@
+package org.hqf.tutorials.java.basic;
+
+public class BooleanDemo {
+
+    public static void main(String[] args) {
+        Byte shortageReceiveFlag = 1;
+        boolean boolValue = Boolean.parseBoolean(shortageReceiveFlag.toString());
+        System.out.println("boolValue = " + boolValue);
+
+        boolValue = !shortageReceiveFlag.equals(0);
+        System.out.println("boolValue = " + boolValue);
+
+        shortageReceiveFlag=null;
+        boolValue = !shortageReceiveFlag.equals(0);
+        System.out.println("boolValue = " + boolValue);
+
+
+    }
+}
+
+
