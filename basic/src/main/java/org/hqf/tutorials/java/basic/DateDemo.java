e87072fd9acbf3d19da8a65f9edfd8af2e536b54
huoxudong125 <423huo@163.com>
2018-03-15 12:09:31 +0800

    Add DateDemo

---
+++ basic/src/main/java/org/hqf/tutorials/java/basic/DateDemo.java
@@ -0,0 +1,13 @@
+package org.hqf.tutorials.java.basic;
+
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String[] args) {
+
+        //1521085914747
+        System.out.println(new Date().getTime());
+        //1521085914747
+        System.out.println(System.currentTimeMillis());
+    }
+}
