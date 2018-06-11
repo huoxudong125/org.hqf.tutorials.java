6317e6263d8624604314a84424f4c1252ab60532
huoxudong125 <423huo@163.com>
2018-06-11 21:47:12 +0800

    fix apache utils

--- utils/src/main/java/org/hqf/tutorials/java/utils/apache.java
+++ utils/src/main/java/org/hqf/tutorials/java/utils/apache.java
@@ -1,5 +1,6 @@
 package org.hqf.tutorials.java.utils;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.math.NumberUtils;
 
 import java.math.BigDecimal;
@@ -30,11 +31,49 @@ public class apache {
         BigDecimal doubleNumE=new BigDecimal(numberStrWithE.trim());
         System.out.println("doubleNumE = " + doubleNumE);
         System.out.println("doubleNumE = " + doubleNumE.toPlainString());
+
+
+
+        String numStr="";
+        BigDecimal bigDecimal=ConvertStrToBigDecimal(numStr);
+        System.out.println("bigDecimal = " + bigDecimal);
+        numStr="1.6215876E7";
+        bigDecimal=ConvertStrToBigDecimal(numStr);
+        System.out.println("bigDecimal = " + bigDecimal);
+
+        numStr="123456789123.123456";
+        bigDecimal=ConvertStrToBigDecimal(numStr);
+        System.out.println("bigDecimal = " + bigDecimal);
+
+
+        numStr="123456789123.123456d";
+        bigDecimal=ConvertStrToBigDecimal(numStr);
+        System.out.println("bigDecimal = " + bigDecimal);
     }
 
     private static void IsNumberStr(String numberStr) {
         boolean isNumber = NumberUtils.isNumber(numberStr);
         System.out.println("numberStr" + numberStr + ",isNumber = " + isNumber);
+    }
+
+
+    private static BigDecimal ConvertStrToBigDecimal(String decimalStr) {
+        BigDecimal result = null;
+
+        if (StringUtils.isNotBlank(decimalStr)) {
+            String trimmedDecimalStr = decimalStr.trim();
+            if (NumberUtils.isNumber(trimmedDecimalStr)) {
+                result = new BigDecimal(trimmedDecimalStr);
+            } else {
+               String msg= String.format("ConvertStrToBigDecimal 字符串转BigDecimal失败，当前参数为【%s】", decimalStr);
+                System.out.println("msg = " + msg);
+            }
+        } else {
+           String msg=String.format("ConvertStrToBigDecimal 字符串转BigDecimal失败，当前参数为【%s】", decimalStr);
+            System.out.println("msg = " + msg);
+        }
+
+        return result;
     }
 
 }
