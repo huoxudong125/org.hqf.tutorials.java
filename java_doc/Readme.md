6a0b175cdc6f429d0b97a814b7c9861ba47c9b7c
huoxudong125 <423huo@163.com>
2018-07-23 22:15:30 +0800

    Add Java Doc Demos

---
+++ java_doc/Readme.md
@@ -0,0 +1,2 @@
+
+在 “Tools->Gerenate JavaDoc” 面版的 “Other command line arguments:” 栏里输入 “-encoding utf-8 -charset utf-8”.
---
+++ java_doc/src/main/java/org/hqf/java/doc/JavaDemo.java
@@ -0,0 +1,47 @@
+package org.hqf.java.doc;
+
+/**
+ * 类的说明
+ * <p>
+ *     javadocDemo 类演示如何生成类的API文档<br>
+ *     @author hqf
+ *     @version 1.0 ldd 2017.3.11
+ * </p>
+ */
+public class JavaDemo {
+    /*对于类的公有属性，也要写上相关注释*/
+    /**
+     * 用于显示一个提示信息
+     */
+    public final String message="this is a demo for javadocument.";
+    /**
+     * 这个方法是程序入口，虚拟机载入这个类的时候，
+     * 将从这个类开始运行程序
+     * @param args commandlines<br>
+     */
+    public static  void main(String []args){
+        JavaDemo demo=new JavaDemo();
+        System.out.println(demo.message);
+        System.out.print(demo.upcaseMessage());
+        System.out.print(demo.getChars(2,6));
+    }
+
+    /**
+     * 从message中获取指定的字串
+     * @param i 子串的开始下标
+     * @param i1  子串的结束目标
+     * @return   从开始到结束的子串
+     */
+    private String getChars(int i, int i1) {
+        return message.substring(i,i1);
+    }
+
+    /**
+     * 将message转化成大写的字符串
+     * @return   大写的字符串
+     */
+    private String upcaseMessage() {
+        return message.toUpperCase();
+    }
+
+}
