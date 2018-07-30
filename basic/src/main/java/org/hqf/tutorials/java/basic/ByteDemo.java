80850a36e2c158c05525b3864764f6c6d0581586
huoxudong125 <423huo@163.com>
2018-07-30 16:21:28 +0800

    Add Java Byte equal Enums

--- basic/src/main/java/org/hqf/tutorials/java/basic/ByteDemo.java
+++ basic/src/main/java/org/hqf/tutorials/java/basic/ByteDemo.java
@@ -14,6 +14,20 @@ public class ByteDemo {
         System.out.println("a = " + a);
         changeByte(a);
         System.out.println("after changeByte(a), a= " + a);
+        Byte c=Byte.parseByte("1");
+        changeByte(c);
+        System.out.println("after changeByte(c), c= " + c);
+
+        System.out.println("a==c "+(a==c));
+        System.out.println("a.eaual(c) "+a.equals(c));
+
+
+        DemoA demoA1 =new DemoA();
+        demoA1.setYn((byte)1);
+        DemoB demoB1 =new DemoB();
+        demoB1.setYn(Byte.parseByte("1"));
+        System.out.println("demoA1==demoB1 "+(demoA1.getYn()== demoB1.getYn()));
+        System.out.println("demoA1.eaual(demoB1)  "+demoA1.getYn().equals(demoB1.getYn()));
 
     }
 
@@ -21,8 +35,34 @@ public class ByteDemo {
         //TODO
         b++;
         System.out.println("b = " + b);
+
     }
 
+
+}
+
+class DemoA {
+    Byte yn;
+
+    public Byte getYn() {
+        return yn;
+    }
+
+    public void setYn(Byte yn) {
+        this.yn = yn;
+    }
+
+}
+class DemoB {
+    Byte yn;
+
+    public Byte getYn() {
+        return yn;
+    }
+
+    public void setYn(Byte yn) {
+        this.yn = yn;
+    }
 }
 
 
