572d89915249e3801cd49285641153e84f04b092
huoxudong125 <423huo@163.com>
2018-08-20 10:28:52 +0800

    Add Java 导出Excel和导入excel

--- aop/src/main/java/org/hqf/tutorials/java/logger/LogAspect.java
+++ aop/src/main/java/org/hqf/tutorials/java/logger/LogAspect.java
@@ -2,6 +2,10 @@ package org.hqf.tutorials.java.logger;
 
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import javassist.ClassClassPath;
+import javassist.ClassPool;
+import javassist.CtMethod;
+import javassist.bytecode.LocalVariableAttribute;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -12,8 +16,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by junbaor on 2017/5/12.
@@ -74,5 +82,50 @@ public class LogAspect {
         }
         return null;
     }
+
+
+    /**
+     * 获取方法入参变量名
+     *
+     * @param clazz
+     *            触发的类
+     * @param methodName
+     *            触发的方法名
+     * @param include
+     *            需要打印的变量名
+     * @return
+     * @throws Exception
+     */
+    private Map<String, Object> getMethodParamNames(Class clazz,
+                                                    String methodName, String include) throws Exception {
+        ClassPool pool = ClassPool.getDefault();
+        pool.insertClassPath(new ClassClassPath(clazz));
+        CtMethod cm = pool.get(clazz.getName()).getDeclaredMethod(methodName);
+        LocalVariableAttribute attr = (LocalVariableAttribute) cm
+                .getMethodInfo().getCodeAttribute()
+                .getAttribute(LocalVariableAttribute.tag);
+
+        if (attr == null) {
+            throw new Exception("attr is null");
+        } else {
+            Map<String, Object> paramNames = new HashMap<String, Object>();
+            int paramNamesLen = cm.getParameterTypes().length;
+            int pos = Modifier.isStatic(cm.getModifiers()) ? 0 : 1;
+            if (StringUtils.isEmpty(include)) {
+                for (int i = 0; i < paramNamesLen; i++) {
+                    paramNames.put(attr.variableName(i + pos), i);
+                }
+            } else { // 若include不为空
+                for (int i = 0; i < paramNamesLen; i++) {
+                    String paramName = attr.variableName(i + pos);
+                    if (include.indexOf(paramName) > -1) {
+                        paramNames.put(paramName, i);
+                    }
+                }
+            }
+            return paramNames;
+        }
+    }
+
 
 }
---
+++ officeUtils/src/main/java/org/hqf/tutorials/java/excel/Demo.java
@@ -0,0 +1,175 @@
+package org.hqf.tutorials.java.excel;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author huoquanfu
+ * @date 2018/08/20
+ */
+public class Demo {
+
+    public static void main(String args[]) throws IOException {
+
+        //Create an array with the data in the same order in which you //expect to be filled in excel file
+        String[] valueToWrite = {"abc@gmail.com", "Pass@123"};
+
+        //Create an object of current class
+        Demo objExcelFile = new Demo();
+
+        //Write the file using file name, sheet name and the data to be filled
+        objExcelFile.writeExcel(System.getProperty("user.dir") + "\\testData", "data.xlsx", "LoginDataSheet", valueToWrite);
+
+        //Prepare the path of excel file
+
+        String filePath = System.getProperty("user.dir") + "\\testData";
+
+        //Call read file method of the class to read data
+
+        objExcelFile.readExcelFile(filePath, "data.xlsx", "LoginDataSheet");
+
+    }
+
+    public void readExcelFile(String filePath, String fileName, String sheetName) throws IOException {
+
+        //Create an object of File class to open xlsx file
+
+        File file = new File(filePath + "\\" + fileName);
+
+        //Create an object of FileInputStream class to read excel file
+
+        FileInputStream inputStream = new FileInputStream(file);
+
+        Workbook workbook = null;
+
+        //Find the file extension by splitting file name in substring  and getting only extension name
+
+        String fileExtensionName = fileName.substring(fileName.indexOf("."));
+
+        //Check condition if the file is xlsx file
+
+        if (fileExtensionName.equals(".xlsx")) {
+
+            //If it is xlsx file then create object of XSSFWorkbook class
+
+            workbook = new XSSFWorkbook(inputStream);
+
+        }
+
+        //Check condition if the file is xls file
+
+        else if (fileExtensionName.equals(".xls")) {
+
+            //If it is xls file then create object of XSSFWorkbook class
+
+            workbook = new HSSFWorkbook(inputStream);
+
+        }
+
+        //Read sheet inside the workbook by its name
+
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        //Find number of rows in excel file
+
+        int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+
+        //Create a loop over all the rows of excel file to read it
+
+        for (int i = 0; i < rowCount + 1; i++) {
+
+            Row row = sheet.getRow(i);
+
+            //Create a loop to print cell values in a row
+
+            for (int j = 0; j < row.getLastCellNum(); j++) {
+
+                //Print Excel data in console
+
+                System.out.print(row.getCell(j).getStringCellValue() + "|| ");
+
+            }
+        }
+    }
+
+    public void writeExcel(String filePath, String fileName, String sheetName, String[] dataToWrite) throws IOException {
+
+        //Create an object of File class to open xlsx file
+
+        File file = new File(filePath + "\\" + fileName);
+
+        //Create an object of FileInputStream class to read excel file
+        FileInputStream inputStream = new FileInputStream(file);
+
+        Workbook qamantraWorkbook = null;
+        //Find the file extension by splitting  file name in substring // and getting only extension name
+
+        String fileExtensionName = fileName.substring(fileName.indexOf("."));
+
+        //Check condition if the file is xlsx file
+
+        if (fileExtensionName.equals(".xlsx")) {
+
+            //If it is xlsx file then create object of XSSFWorkbook class
+            qamantraWorkbook = new XSSFWorkbook(inputStream);
+
+        }
+
+        //Check condition if the file is xls file
+
+        else if (fileExtensionName.equals(".xls")) {
+
+            //If it is xls file then create object of XSSFWorkbook class
+
+            qamantraWorkbook = new HSSFWorkbook(inputStream);
+
+        }
+
+
+        //Read excel sheet by sheet name
+        Sheet sheet = qamantraWorkbook.getSheet(sheetName);
+
+        //Get the current count of rows in excel file
+        int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+
+        //Get the first row from the sheet
+        Row row = sheet.getRow(0);
+
+        //Create a new row and append it at last of sheet
+        Row newRow = sheet.createRow(rowCount + 1);
+
+        //Create a loop over the cell of newly created Row
+//        for (int j = 0; j < row.getLastCellNum(); j++) {
+        for (int j = 0; j < dataToWrite.length; j++) {
+
+            //Fill data in row
+            Cell cell = newRow.createCell(j);
+            cell.setCellValue(dataToWrite[j]);
+
+        }
+
+        //Close input stream
+        inputStream.close();
+
+        //Create an object of FileOutputStream class to create write data //in excel file
+        FileOutputStream outputStream = new FileOutputStream(file);
+
+        //write data in the excel file
+
+        qamantraWorkbook.write(outputStream);
+
+        //close output stream
+
+        outputStream.close();
+
+    }
+}
