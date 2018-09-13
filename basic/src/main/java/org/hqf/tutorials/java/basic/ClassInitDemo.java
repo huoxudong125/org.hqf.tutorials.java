7c25af9eb3fc1712faa3f2eba86b03bd5af11e07
huoxudong125 <423huo@163.com>
2018-09-13 15:20:55 +0800

    Add Java 读取指定的列

--- basic/src/main/java/org/hqf/tutorials/java/basic/ClassInitDemo.java
+++ basic/src/main/java/org/hqf/tutorials/java/basic/ClassInitDemo.java
@@ -6,15 +6,23 @@ package org.hqf.tutorials.java.basic;
  */
 public class ClassInitDemo {
     public static void main(String[] args) {
+        System.out.println();
         System.out.println("A.staticStrA = " + A.staticStrA);
-        System.out.println("A.staticStrA = " + B.staticStrB);
+        System.out.println();
+        System.out.println("B.staticStrB = " + B.staticStrB);
+        System.out.println();
+        System.out.println("C.staticStrC = " + C.staticStrC);
+        System.out.println();
+        System.out.println("C2.staticStrP = " + C2.staticStrP);
+        System.out.println();
+
     }
 
 }
 
 
- class A{
-    public static final String staticStrA="staticStrA";
+class A {
+    public static final String staticStrA = "staticStrA";
 
     static {
         System.out.println("Class A Static init");
@@ -22,14 +30,43 @@ public class ClassInitDemo {
 }
 
 
-
-class B{
+class B {
     static {
         System.out.println("Class B Static init");
     }
 
-    public static final String staticStrB="staticStrB";
+    public static final String staticStrB = new String("staticStrB");
 }
+
+class P {
+    static {
+        System.out.print("P");
+    }
+}
+
+class C extends P {
+    static {
+        System.out.print("C");
+    }
+
+    public final static String staticStrC = "C";
+}
+
+class P2 {
+    public  static String staticStrP = "staticStrP2";
+
+    static {
+        System.out.print("P2");
+    }
+}
+
+class C2 extends P2 {
+
+    static {
+        System.out.print("C2");
+    }
+}
+
 
 
 
